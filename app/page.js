5c39aea15522e38028dff5517b27144406aa575f
Josue Ramos <nullrootbyte@gmail.com>
2023-09-01 10:31:58 -0600

    Mes patrio

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ import moment from "moment";
 export default function Home() {
   const backgroundImage = 'Gondola_Josue_Angela.svg';
 
-  const nextDate = moment('2023-10-04 07:50:00');
+  const nextDate = moment('2023-9-14 07:50:00');
   const [days, setDays] = useState(0);
   const [hours, setHours] = useState(0);
   const [minutes, setMinutes] = useState(0);
@@ -114,49 +114,7 @@ export default function Home() {
     };
   }, []);
   const Messages = [
-    "En el encuentro de dos almas, se despiertan infinitas posibilidades.",
-    "La distancia entre tú y yo solo existe en la ilusión de la separación.",
-    "Eres como un espejo, reflejando la belleza que hay en mí.",
-    "El amor es un misterio, un laberinto sin fin de conexiones y significados.",
-    "Cada instante de espera es un peldaño que nos acerca al reencuentro.",
-    "Eres un enigma que me cautiva, una pregunta sin respuesta.",
-    "El tiempo es solo una ilusión, nuestro amor trasciende las dimensiones.",
-    "En cada mirada se revela el universo que vive en tus ojos.",
-    "La esencia de la vida es fluir como un río, sin resistencia ni apego.",
-    "Eres un sueño dentro de un sueño, una realidad tejida de fantasías.",
-    "Nuestro amor es un eco en el tiempo, resonando en el corazón del universo.",
-    "En el abrazo del infinito, nuestros corazones encuentran su hogar.",
-    "Eres un universo por descubrir, con galaxias de emociones y pensamientos.",
-    "La existencia es un baile cósmico, donde nuestras almas se entrelazan.",
-    "En cada palabra compartida se forja el entendimiento de dos seres.",
-    "El amor es un viaje eterno, en el cual siempre estamos en camino.",
-    "Eres una melodía en la sinfonía del cosmos, un acorde perfecto.",
-    "La eternidad se revela en un instante, cuando nuestros corazones se encuentran.",
-    "Somos como hojas flotando en el río del destino, navegando juntos.",
-    "En cada encuentro, el universo conspira para unir nuestras almas.",
-    "Eres el enigma que da sentido a mi existencia, un puzle por resolver.",
-    "La conexión entre tú y yo trasciende cualquier lógica o razón.",
-    "Eres la danza del ser, en el fluir constante del tiempo y el espacio.",
-    "En cada suspiro, nos sumergimos en el océano de lo desconocido.",
-    "El amor es una búsqueda infinita, donde cada encuentro es un hallazgo.",
-    "Eres una pregunta sin respuesta, una búsqueda incesante de significado.",
-    "La vida es un lienzo en blanco, donde escribimos nuestra historia juntos.",
-    "En el tejido del destino, nuestros hilos se entrelazan con propósito.",
-    "Eres el reflejo de mi alma, en el espejo de la existencia.",
-    "El amor es como un río que fluye sin cesar, llevándonos hacia lo desconocido.",
-    "En cada pensamiento, encuentro una razón para amarte más.",
-    "Eres la pregunta que da sentido a mi búsqueda, un enigma por resolver.",
-    "La vida es una danza cósmica, donde nuestros pasos están sincronizados.",
-    "En cada latido, se dibuja la geometría del amor en el universo.",
-    "Eres la melodía que mece mi corazón, en el ritmo de la existencia.",
-    "El amor es un enigma divino, donde cada respuesta conduce a una nueva pregunta.",
-    "En el silencio del universo, nuestras almas se encuentran sin palabras.",
-    "Eres una partícula en el tejido del cosmos, conectada con todo lo que existe.",
-    "La esencia del amor es el encuentro de dos almas en el eterno ahora.",
-    "En cada encuentro, la sincronicidad del universo nos une en un solo latido.",
-    "Eres el eco de mi ser, resonando en los confines del universo.",
-    "El amor es una danza eterna, donde nuestros pasos se funden en un solo compás.",
-    "En cada pensamiento, encuentro la poesía de tu existencia.",
+   
     "Eres la melodía que mece mi alma, en el concierto del cosmos.",
     "La vida es una obra maestra, donde tú eres mi musa y mi inspiración.",
     "En el lienzo de la realidad, pintamos nuestro amor con colores eternos.",
